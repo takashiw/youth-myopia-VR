da7067a138f85439fa7ea27f147e446d69218979
iTwicKz <twickes32@ufl.edu>
2017-09-19 12:24:13 -0700

    Completed Raycast and FPS camera

---
+++ demo-myopic-classroom/Assets/LineOfSight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour {
+
+	private RaycastHit vision;
+	public float rayLength;
+	public bool isGrabbed;
+	private Rigidbody grabbedObject;
+
+	// Use this for initialization
+	void Start () {
+		rayLength = 4.0f;
+		isGrabbed  = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawRay (Camera.main.transform.position, Camera.main.transform.forward * rayLength, Color.red, 0.5f);
+
+		if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out vision, rayLength)) {
+			if(vision.collider.tag == "Interactive"){
+				Debug.Log(vision.collider.name);
+			}
+
+		}
+	}
+}
